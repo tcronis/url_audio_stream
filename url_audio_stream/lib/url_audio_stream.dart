07d3546db163a1033da7fbde1b402fe3c118c8db
tcronis <46980360+tcronis@users.noreply.github.com>
2019-11-09 17:05:08 -0600

    Creation of the dart class for calling the channel to native

--- url_audio_stream/lib/url_audio_stream.dart
+++ url_audio_stream/lib/url_audio_stream.dart
@@ -2,12 +2,61 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class UrlAudioStream {
-  static const MethodChannel _channel =
-      const MethodChannel('url_audio_stream');
+class AudioStream{
+  String _url;
+  static const MethodChannel _channel = const MethodChannel('url_audio_stream');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  //constructor
+  AudioStream(String url){
+    this._url = url;
   }
+
+  //method start an audio stream
+  Future <void> start() async{
+    try{
+      await _channel.invokeMethod(_url.toString(), {"method" : "start"});
+    } on PlatformException catch (e){
+      print("Stream start error : $e");
+    }
+  }
+
+  //method to stop an audio stream
+  Future <void> stop() async{
+    try{
+      await _channel.invokeMethod(_url.toString(), {"method" : "stop"});
+    } on PlatformException catch (e){
+      print("Stream stop error : $e");
+    }
+  }
+
+  //method to pause an audio stream
+  Future <void> pause() async{
+    try{
+      await _channel.invokeMethod(_url.toString(), {"method" : "pause"});
+    } on PlatformException catch (e){
+      print("Stream pause error : $e");
+    }
+  }
+
+  //method to resume an audio stream
+  Future <void> resume() async{
+    try{
+      await _channel.invokeMethod(_url.toString(), {"method" : "resume"});
+    } on PlatformException catch (e){
+      print("Stream resume error : $e");
+    }
+  }
+
 }
+
+
+
+// class UrlAudioStream {
+//   static const MethodChannel _channel =
+//       const MethodChannel('url_audio_stream');
+
+//   static Future<String> get platformVersion async {
+//     final String version = await _channel.invokeMethod('getPlatformVersion');
+//     return version;
+//   }
+// }
